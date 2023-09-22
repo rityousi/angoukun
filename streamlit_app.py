bb2bb99f9ff484a181384608c434c1e79cc82e1c
rityousi <144981077+rityousi@users.noreply.github.com>
2023-09-22 07:38:35 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,20 @@ st.set_page_config(page_title="タイトル", layout="wide")
 import streamlit as st
 import time
 import math
-
+ 
 def factorize(n):
     factors = []
     while n % 2 == 0:
         factors.append(2)
         n //= 2
-    for i in range(2, int(math.sqrt(n))+1):
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         while n % i == 0:
             factors.append(i)
             n //= i
     if n > 1:
         factors.append(n)
     return factors
-
+ 
 def main():
     st.title("素因数分解時間計測アプリ")
     st.write("このアプリは、与えられた整数の素因数分解を行い、所要時間を計測します。")
@@ -34,7 +34,7 @@ def main():
         end_time = time.time()
         elapsed_time = end_time - start_time
         st.write(f"素因数分解結果: {factors}")
-        st.write(f"素因数分解にかかる時間: {elapsed_time:.4f}秒")
+        st.write(f"素因数分解にかかる時間: {elapsed_time:.4f} 秒")
     if st.button("10回計測"):
         elapsed_times = []
         for i in range(10):
@@ -45,7 +45,7 @@ def main():
             elapsed_times.append(elapsed_time)
         avg_elapsed_time = sum(elapsed_times) / len(elapsed_times)
         st.write(f"素因数分解結果: {factors}")
-        st.write(f"素因数分解にかかる平均時間: {avg_elapsed_time:.4f}秒")
-
+        st.write(f"素因数分解にかかる平均時間: {avg_elapsed_time:.4f} 秒")
+ 
 if __name__ == "__main__":
     main()
