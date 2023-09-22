9ea07361d7939c8d51db10e1253ec1e85fe99bc9
rityousi <144981077+rityousi@users.noreply.github.com>
2023-09-22 08:06:15 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ st.set_page_config(page_title="タイトル", layout="wide")
 import streamlit as st
 import time
 import math
+import random
  
 def factorize(n):
     factors = []
@@ -47,5 +48,27 @@ def main():
         st.write(f"素因数分解結果: {factors}")
         st.write(f"素因数分解にかかる平均時間: {avg_elapsed_time:.4f} 秒")
  
+if __name__ == "__main__":
+    main()
+    def main():
+    st.write("I'm thinking of a number between 1 and 100. Can you guess what it is?")
+    
+    # Generate a random number between 1 and 100
+    number_to_guess = random.randint(1, 100)
+    
+    # Allow the user to make up to 5 guesses
+    for i in range(5):
+        guess = st.number_input(f"Guess #{i+1}", min_value=1, max_value=100)
+        if guess == number_to_guess:
+            st.write("Congratulations! You guessed the number!")
+            return
+        elif guess < number_to_guess:
+            st.write("Too low! Try again.")
+        else:
+            st.write("Too high! Try again.")
+    
+    # If the user hasn't guessed the number after 5 tries, reveal the answer
+    st.write(f"Sorry, you didn't guess the number. The number was {number_to_guess}.")
+
 if __name__ == "__main__":
     main()
