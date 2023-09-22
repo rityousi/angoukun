2d19c4895e665f61e9ea5e25fe548d51ca04d3ac
rityousi <144981077+rityousi@users.noreply.github.com>
2023-09-22 00:02:49 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,14 @@ def main():
     st.title("素因数分解時間計測アプリ")
     st.write("このアプリは、与えられた整数の素因数分解を行い、所要時間を計測します。")
     number_to_factorize = st.number_input("素因数分解対象の数を入力してください:", min_value=1)
-    if st.button("計測開始"):
+    if st.button("1回計測"):
+        start_time = time.time()
+        factors = factorize(number_to_factorize)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        st.write(f"素因数分解結果: {factors}")
+        st.write(f"素因数分解にかかる時間: {elapsed_time:.4f} 秒")
+    if st.button("10回計測"):
         elapsed_times = []
         for i in range(10):
             start_time = time.time()
