b7f937ceec282c6a4cde805f115cd77522cb99a0
rityousi <144981077+rityousi@users.noreply.github.com>
2023-09-22 08:22:04 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ while n %2==0:
         factors.append(2)
         n //=2
 for i in range(3, int(math.sqrt(n)) +1, 2):
-while n % i ==0:
+while n % i == 0:
             factors.append(i)
             n //= i
 if n >1:
